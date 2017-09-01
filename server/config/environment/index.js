955bcbe063d088d1cee8def1e7aa8abd0feb4a9f
unknown <burnsoft@gmail.com>
2017-09-01 14:32:17 -0500

    dockerized and dep cleanup

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -30,36 +30,6 @@ var all = {
   // Secret for session, you will want to change this and make it an environment variable
   secrets: {
     session: 'webglDiceRoller2-secret'
-  },
-
-  // List of user roles
-  userRoles: ['guest', 'user', 'admin'],
-
-  // MongoDB connection options
-  mongo: {
-    options: {
-      db: {
-        safe: true
-      }
-    }
-  },
-
-  facebook: {
-    clientID:     process.env.FACEBOOK_ID || 'id',
-    clientSecret: process.env.FACEBOOK_SECRET || 'secret',
-    callbackURL:  (process.env.DOMAIN || '') + '/auth/facebook/callback'
-  },
-
-  twitter: {
-    clientID:     process.env.TWITTER_ID || 'id',
-    clientSecret: process.env.TWITTER_SECRET || 'secret',
-    callbackURL:  (process.env.DOMAIN || '') + '/auth/twitter/callback'
-  },
-
-  google: {
-    clientID:     process.env.GOOGLE_ID || 'id',
-    clientSecret: process.env.GOOGLE_SECRET || 'secret',
-    callbackURL:  (process.env.DOMAIN || '') + '/auth/google/callback'
   }
 };
 
